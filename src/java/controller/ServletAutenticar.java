4b89fd6c089471a1483e97000f72e12d4dd74a98
T-102 <T-102@PC201>
2017-03-18 10:38:28 -0600

    Listo

---
+++ src/java/controller/ServletAutenticar.java
@@ -0,0 +1,26 @@
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletAutenticar extends HttpServlet {
+
+   
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("Application/vnd.ms-excel");
+       PrintWriter salida=  response.getWriter();
+        salida.println("Yo soy Michele ");
+           
+        }
+    }
+
+    
+
+
