9a35c4b51eae5cbbbd8365e49c2fff34305dffac
T-102 <T-102@PC201>
2017-04-01 10:40:55 -0600

    clase de hoy 0104

--- src/java/model/ModeloConexion.java
+++ src/java/model/ModeloConexion.java
@@ -1,12 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-//importamo el paquete oara usar oracle y su conector
+//importamos el paquete oara usar oracle y su conector
 import java.sql.*;
+
+
 public class ModeloConexion {
-    
+     public static Connection conectarse(String login, 
+           String password)throws Exception{
+       
+       String url="jdbc:orcle:thin@localhost:1521:orcl";
+       Class.forName("oracle.jdbc.OracleDriver");
+      Connection con= DriverManager.getConnection(url,login, password);
+       return con;
+   }
 }
